269180fe0b7497750551972ad8d6d718561d7b1f
xpymka777 <gumangu12@gmail.com>
2023-09-18 11:12:58 +0700

    закончил 5 шаг

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
-
+from django.db.models import Q
 # Create your views here.
+
+from .models import Book, Author, BookInstance, Genre
+
+
+# изначальная функция отображения
+# def index(request):
+#     """
+#     Функция отображения для домашней страницы сайта.
+#     """
+#     # Генерация "количеств" некоторых главных объектов
+#     num_books = Book.objects.all().count()
+#     num_instances = BookInstance.objects.all().count()
+#     # Доступные книги (статус = 'a')
+#     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+#     num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+#
+#     # Отрисовка HTML-шаблона index.html с данными внутри
+#     # переменной контекста context
+#     return render(
+#         request,
+#         'index.html',
+#         context={'num_books': num_books, 'num_instances': num_instances,
+#                  'num_instances_available': num_instances_available, 'num_authors': num_authors},
+#     )
+
+# новая функция отображения, которая вроде ничего не ломает
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+
+    # Получение количества жанров
+    num_genres = Genre.objects.count()
+
+    # Получение количества книг, содержащих определенное слово в заголовке
+    search_word = "ваше_слово_для_поиска"
+    num_books_with_word = Book.objects.filter(title__icontains=search_word).count()
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors,
+            'num_genres': num_genres,
+            'num_books_with_word': num_books_with_word,
+        },
+    )
